e71b17f91f1feffb49ff953c2023ac95d333a42e
Suraj Kumar Sharma <sysnicpro123@gmail.com>
2023-08-29 16:32:56 +0530

    error page

--- src/pages/404Page/PageNotFound.jsx
+++ src/pages/404Page/PageNotFound.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Wrapper from '../../components/wrapper/Wrapper';
+
 import './pageNotFound.css';
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="pageNotFound">
+      <Wrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </Wrapper>
+    </div>
+  );
 };
 
 export default PageNotFound;
